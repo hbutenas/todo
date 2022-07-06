a6404b4c6fafb82541a6609437194d152acc33b2
Henrikas Butenas <butenashenrikas@gmail.com>
2022-07-06 16:19:03 +0300

    added app guard to protect private routing

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,8 +2,16 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
+import { APP_GUARD } from '@nestjs/core';
+import { AccessTokenGuard } from './auth/guards';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, AuthModule],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AccessTokenGuard, // If there is no @Public() decorator it will require a access token on entry
+    },
+  ],
 })
 export class AppModule {}
