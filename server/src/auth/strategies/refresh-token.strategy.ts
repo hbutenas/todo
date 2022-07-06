c7001abd5ceb52fd9852331d8937b6e20102f2fd
Henrikas Butenas <butenashenrikas@gmail.com>
2022-07-06 09:49:00 +0300

    strategies created

---
+++ server/src/auth/strategies/refresh-token.strategy.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+import { Request } from 'express';
+
+@Injectable()
+export class RefreshTokenJwtStrategy extends PassportStrategy(Strategy, 'jwt-refresh') {
+  constructor(config: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: config.get('REFRESH_TOKEN'),
+      passReqToCallback: true,
+    });
+  }
+
+  validate(req: Request, payload: any) {
+    const refreshToken = req.get('authorization').replace('Bearer', '').trim();
+    return { ...payload, refreshToken };
+  }
+}
