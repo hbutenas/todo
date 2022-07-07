3d951f57355b56683e2651b6ea56eea58639f16b
Henrikas Butenas <butenashenrikas@gmail.com>
2022-07-07 11:07:11 +0300

    auth functionallity completed

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,19 +1,39 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { RegisterDto } from './dto';
+import { GetCurrentUser, Public } from './decorators';
+import { GetCurrentUserId } from './decorators/get-current-user-id.decorator';
+import { RegisterDto, LoginDto } from './dto';
+import { RefreshTokenGuard } from './guards';
 
 @Controller('api/v1/auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @Public()
   @Post('register')
+  @HttpCode(HttpStatus.CREATED)
   public async register(@Body() body: RegisterDto) {
     return this.authService.register(body);
   }
 
+  @Public()
   @Post('login')
-  public async login() {}
+  @HttpCode(HttpStatus.OK)
+  public async login(@Body() body: LoginDto) {
+    return this.authService.login(body);
+  }
 
   @Post('logout')
-  public async logout() {}
+  @HttpCode(HttpStatus.OK)
+  public async logout(@GetCurrentUserId() userId: number) {
+    return this.authService.logout(userId);
+  }
+
+  @Public()
+  @UseGuards(RefreshTokenGuard)
+  @Post('refresh')
+  @HttpCode(HttpStatus.OK)
+  public async refresh(@GetCurrentUserId() userId: number, @GetCurrentUser('refreshToken') refreshToken: string) {
+    return this.authService.refresh(userId, refreshToken);
+  }
 }
--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,14 +1,16 @@
-import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { BadRequestException, ForbiddenException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { RegisterDto } from './dto';
+import { RegisterDto, LoginDto } from './dto';
 import * as bcrypt from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { Tokens } from './types';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService, private jwt: JwtService) {}
+  constructor(private prisma: PrismaService, private jwt: JwtService, private config: ConfigService) {}
 
-  public async register(body: RegisterDto) {
+  public async register(body: RegisterDto): Promise<Tokens> {
     // Check for already existing user
     const existingUser = await this.prisma['User'].findFirst({
       where: {
@@ -22,7 +24,7 @@ export class AuthService {
     }
 
     // Hash the password
-    const hashedPassword = await this.generatePasswordHash(body.password);
+    const hashedPassword = await this.hashData(body.password);
 
     // Create the user
     const user = await this.prisma['User'].create({
@@ -41,17 +43,144 @@ export class AuthService {
     }
 
     // Generate tokens
+    const tokens = await this.generateTokens(user.id, user.email);
+
+    // Update the refresh token in db
+    await this.updateRefreshToken(user.id, tokens.refresh_token);
+
+    // Return tokens
+    return tokens;
   }
 
-  public async login() {}
+  public async login(body: LoginDto): Promise<Tokens> {
+    // Check for existing user
+    const user = await this.prisma['User'].findFirst({
+      where: {
+        email: body.email,
+      },
+    });
 
-  public async logout() {}
+    // If couldn't find the user throw an error
+    if (!user) {
+      throw new BadRequestException('Invalid email address or password');
+    }
 
-  private async generatePasswordHash(password): Promise<string> {
-    return await bcrypt.hash(password, 10);
+    // User exists, compare the password
+    const matchingPassword = await bcrypt.compare(body.password, user.password);
+
+    // If password does not match throw an error
+    if (!matchingPassword) {
+      throw new BadRequestException('Invalid email address or password');
+    }
+
+    // Generate tokens
+    const tokens = await this.generateTokens(user.id, user.email);
+
+    // Update the refresh token in db
+    await this.updateRefreshToken(user.id, tokens.refresh_token);
+
+    // Return tokens
+    return tokens;
   }
 
-  private async generateTokens(userId: number, email: string): Promise<string> {
-    return '';
+  public async logout(userId: number): Promise<boolean> {
+    // Find user by provided id and by the refreshToken where is not null and set it to null
+    await this.prisma['User'].updateMany({
+      where: {
+        id: userId,
+        refreshToken: {
+          not: null,
+        },
+      },
+      data: {
+        refreshToken: null,
+      },
+    });
+
+    return true;
+  }
+
+  public async refresh(userId: number, refreshToken: string) {
+    // Find the user
+    const user = await this.prisma['User'].findFirst({
+      where: {
+        id: userId,
+      },
+    });
+
+    // If user not found or the token is already null
+    if (!user || !user.refreshToken) {
+      throw new ForbiddenException('Access denied');
+    }
+
+    // Compare tokens
+    const refreshTokenMatches = await this.compareToken(refreshToken, user.refreshToken);
+
+    if (!refreshTokenMatches) {
+      throw new ForbiddenException('Access denied');
+    }
+
+    // Generate tokens
+    const tokens = await this.generateTokens(user.id, user.email);
+
+    // Update the refresh token in db
+    await this.updateRefreshToken(user.id, tokens.refresh_token);
+
+    // Return tokens
+    return tokens;
+  }
+
+  // Helpers
+  private async hashData(data: string): Promise<string> {
+    return await bcrypt.hash(data, 10);
+  }
+
+  private async compareToken(providedToken: string, actualToken: string): Promise<boolean> {
+    return await bcrypt.compare(providedToken, actualToken);
+  }
+
+  private async generateTokens(userId: number, email: string): Promise<Tokens> {
+    const [at, rt] = await Promise.all([
+      this.jwt.signAsync(
+        {
+          id: userId,
+          email,
+        },
+        {
+          secret: this.config.get('ACCESS_TOKEN'),
+          expiresIn: 60 * 15, // 15min
+        },
+      ),
+      this.jwt.signAsync(
+        {
+          id: userId,
+          email,
+        },
+        {
+          secret: this.config.get('REFRESH_TOKEN'),
+          expiresIn: 60 * 60 * 24 * 7, // 1week
+        },
+      ),
+    ]);
+
+    return {
+      access_token: at,
+      refresh_token: rt,
+    };
+  }
+
+  private async updateRefreshToken(userId: number, refreshToken: string): Promise<void> {
+    // Hash the token
+    const hashedToken = await this.hashData(refreshToken);
+
+    // Update field with token in db
+    await this.prisma['User'].updateMany({
+      where: {
+        id: userId,
+      },
+      data: {
+        refreshToken: hashedToken,
+      },
+    });
   }
 }
