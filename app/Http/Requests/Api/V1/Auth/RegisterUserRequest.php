73342bb1f80cc704f38952e44a071a592c848688
Henrikas Butenas <butenashenrikas@gmail.com>
2023-03-27 17:18:52 +0300

    set up Request object for registration

--- app/Http/Requests/Api/V1/Auth/RegisterUserRequest.php
+++ app/Http/Requests/Api/V1/Auth/RegisterUserRequest.php
@@ -11,7 +11,7 @@ class RegisterUserRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,8 @@ class RegisterUserRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'email' => ['required', 'email', 'unique:users'],
+            'password' => ['required', 'string', 'min:6'],
         ];
     }
 }
