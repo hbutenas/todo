bf60aeffbf65535c42c5d695c46a0974c45afb85
Henrikas Butenas <butenashenrikas@gmail.com>
2023-03-27 15:38:53 +0300

    HttpResponses created

---
+++ app/Traits/HttpResponses.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Traits;
+
+trait HttpResponses
+{
+    protected function successfullRequest($data, string $message = null, int $code = 200)
+    {
+        return response()->json([
+            'status' => 'Request successful',
+            'message' => $message,
+            'data' => $data
+        ], $code);
+    }
+
+    protected function failedRequest($data, string $message = null, int $code)
+    {
+        return response()->json([
+            'status' => 'Request failed',
+            'message' => $message,
+            'data' => $data
+        ], $code);
+    }
+}
