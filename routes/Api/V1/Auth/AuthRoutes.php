f921b7173fde329986f3729e1ab78ad0d7c4962a
Henrikas Butenas <butenashenrikas@gmail.com>
2023-03-27 15:21:56 +0300

    declared basic auth routes

---
+++ routes/Api/V1/Auth/AuthRoutes.php
@@ -0,0 +1,8 @@
+<?php
+
+use App\Http\Controllers\Api\V1\Auth\AuthController;
+use Illuminate\Support\Facades\Route;
+
+Route::group(['prefix' => 'v1/auth'], function () {
+    Route::post('register', [AuthController::class, 'register']);
+});
