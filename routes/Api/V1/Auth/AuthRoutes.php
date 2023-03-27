948072639d8c9e434efb1130c22926b7c731c8cc
Henrikas Butenas <butenashenrikas@gmail.com>
2023-03-27 17:18:23 +0300

    added login route

--- routes/Api/V1/Auth/AuthRoutes.php
+++ routes/Api/V1/Auth/AuthRoutes.php
@@ -5,4 +5,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'v1/auth'], function () {
     Route::post('register', [AuthController::class, 'register']);
+    Route::post('login', [AuthController::class, 'login']);
 });
