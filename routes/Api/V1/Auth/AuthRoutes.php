15c3551f4d41667c488168c669025411561e7588
Henrikas Butenas <butenashenrikas@gmail.com>
2023-03-30 19:11:48 +0300

    added protected routes

--- routes/Api/V1/Auth/AuthRoutes.php
+++ routes/Api/V1/Auth/AuthRoutes.php
@@ -3,7 +3,13 @@
 use App\Http\Controllers\Api\V1\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
 
+// public routes
 Route::group(['prefix' => 'v1/auth'], function () {
     Route::post('register', [AuthController::class, 'register']);
     Route::post('login', [AuthController::class, 'login']);
+});
+
+// private routes
+Route::group(['prefix' => 'v1/auth', 'middleware' => 'auth:sanctum'], function () {
+    Route::get('identify', [AuthController::class, 'identify']);
 });
