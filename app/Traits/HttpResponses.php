0281a0e6d732f54f7900330cf4f6a18218945bd3
Henrikas Butenas <butenashenrikas@gmail.com>
2023-03-31 16:18:09 +0300

    declared default props

--- app/Traits/HttpResponses.php
+++ app/Traits/HttpResponses.php
@@ -13,7 +13,7 @@ trait HttpResponses
         ], $code);
     }
 
-    protected function failedRequest($data, string $message = null, int $code)
+    protected function failedRequest($data, string $message = null, int $code = 200)
     {
         return response()->json([
             'status' => 'Request failed',
