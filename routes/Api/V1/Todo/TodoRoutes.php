3a33331f93a539e81ffaf16b5d1134aaa3891f89
Henrikas Butenas <butenashenrikas@gmail.com>
2023-04-02 12:12:43 +0300

    all todo routes defined

--- routes/Api/V1/Todo/TodoRoutes.php
+++ routes/Api/V1/Todo/TodoRoutes.php
@@ -6,4 +6,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'v1/todo', 'middleware' => 'auth:sanctum'], function () {
     Route::post('', [TodoController::class, 'store']);
+    Route::get('', [TodoController::class, 'index']);
+    Route::get('{id}', [TodoController::class, 'show'])
+        ->where('id', '[1-9]+');
+    Route::patch('{id}', [TodoController::class, 'update'])
+        ->where('id', '[1-9]+');
+    Route::delete('{id}', [TodoController::class, 'destroy'])
+        ->where('id', '[1-9]+');
 });
