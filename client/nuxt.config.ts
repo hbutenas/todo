23a35289fc44e70778db379bdc4edb2d98140147
Henrikas Butenas <butenashenrikas@gmail.com>
2023-04-03 13:13:50 +0300

    tailwindcss and flowbite setup

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -1,4 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+    modules: [
+        '@nuxtjs/tailwindcss',
+    ],
+    app: {
+        head: {
+            script: [
+                {
+                    hid: 'flowbite',
+                    src: 'https://unpkg.com/flowbite@1.5.3/dist/flowbite.js',
+                    defer: true
+                },
+                {
+                    src: 'https://maps.googleapis.com/maps/api/js?key=AIzaSyAZn3EraQKcexvPaH3Nt0TpnQ_xuV4Dhlg&libraries=places'
+                }
+            ]
+        },
+    },
+});
